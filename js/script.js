5ef761ee901fc61a828277c42dcde267dc118ba3
Marcin Tonder <ginofelino000@gmail.com>
2022-05-30 14:50:46 +0200

    Player movie

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ console.log('Wylosowana liczba to: ' + randomNumber);
 let computerMove = 'nieznany ruch';
 
 if(randomNumber == 1){
-	computerMove = 'kamień';
+	computerMove = 'kamien';
 }
 if(randomNumber == 2){
 	computerMove = 'papier';
@@ -15,3 +15,22 @@ if(randomNumber == 3){
 }
 
 printMessage('Moj ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swoj ruch! 1: kamien, 2: papier, 3: nozyce.');
+
+console.log('Gracz wpisal: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+	playerMove = 'kamien';
+}
+if(playerInput == '2'){
+	playerMove = 'papier';
+}
+if(playerInput == '3'){
+	playerMove = 'nozyce';
+}
+
+printMessage('Twoj ruch to: ' + playerMove);
+
