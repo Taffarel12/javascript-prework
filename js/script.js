650e3015acca007968c271660f80da750f080503
Marcin Tonder <ginofelino000@gmail.com>
2022-05-30 17:37:41 +0200

    if2

--- js/script.js
+++ js/script.js
@@ -37,33 +37,33 @@ if(playerInput == ''){
 
 printMessage('Twoj ruch to: ' + playerMove);
 
-if( computerMove == 'kamien' && playerMove == 'papier'){
+if(computerMove == 'kamien' && playerMove == 'papier'){
 	printMessage('Ty wygrywasz!');
 }
 if(computerMove == 'papier' && playerMove == 'papier'){
 	printMessage('Remis!');
 }
-if( computerMove == 'nozyce' && playerMove == 'papier'){
+if(computerMove == 'nozyce' && playerMove == 'papier'){
 	printMessage('Ty przegrywasz!')
 }
 if(playerMove == 'nieznany ruch'){
 	printMessage('Wykonaj ruch')
 }
-if( computerMove == 'kamien' && playerMove == 'kamien'){
+if(computerMove == 'kamien' && playerMove == 'kamien'){
 	printMessage('Remis!');
 }
 if(computerMove == 'papier' && playerMove == 'kamien'){
 	printMessage('Ty Przegrywasz!');
 }
-if( computerMove == 'nozyce' && playerMove == 'kamien'){
+if(computerMove == 'nozyce' && playerMove == 'kamien'){
 	printMessage('Ty wygrywasz!')
 }
-if( computerMove == 'kamien' && playerMove == 'nozyce'){
+if(computerMove == 'kamien' && playerMove == 'nozyce'){
 	printMessage('Ty przegrywasz!');
 }
 if(computerMove == 'papier' && playerMove == 'nozyce'){
 	printMessage('Ty Wygrywasz!');
 }
-if( computerMove == 'nozyce' && playerMove == 'nozyce'){
+if(computerMove == 'nozyce' && playerMove == 'nozyce'){
 	printMessage('Remis!')
 }
